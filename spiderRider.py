14b584d0708bef0c0f2a417e1f04517da8bd4db6
Toni Butkovic <tonibutkovic23@gmail.com>
2015-06-07 14:02:53 +0200

    Most used words visualization

--- spiderRider.py
+++ spiderRider.py
@@ -1,10 +1,11 @@
-import nltk, re
+import nltk, re, json 
 from nltk.tokenize import RegexpTokenizer
 
 #open file and read, put all word to lowercase so we have no duplicates
 f=open('songLyrics.txt','rU')
 raw=f.read()
 raw = raw.lower()
+f.close()
 
 #add unnecesarry words to remove list and join them for regex
 removeList = ["the", "and", "it", "in", "is", "don", "re", "ll", "ve", "chorus", "na", ]
@@ -22,6 +23,16 @@ tokens = tokenizer.tokenize(raw);
 fdist1 = nltk.FreqDist(tokens);
 words = fdist1.most_common(100);
 
-print words;
+file = open('data.tsv','w')
+file.write("word\tquantity")
 
+mostUsedWord = words[0][1]
+
+for (word, freq) in words:
+    file.write("\n")
+    file.write(word)
+    file.write("\t")
+    file.write(str(freq/float(mostUsedWord)))
+
+file.close()
 
